0d75977cb17401e340588441c7996726fab27594
UP929312 <36114463+UP929312@users.noreply.github.com>
2023-09-10 22:13:18 +0100

    Attempts to get this working

--- setup.py
+++ setup.py
@@ -1,2 +1,22 @@
-from setuptools import setup
-setup()
+from setuptools import setup, find_packages
+
+setup(
+    install_requires=["flake8 > 3.0.0"],
+    name="flake8_pgp",
+    license="MIT",
+    version="1.1.2",
+    description="flake8 plugin which checks for various bad python practises",
+    # author="Ben Skerritt",
+    # author_email="ben.skerrit@vodafone.com",
+    url="https://github.com/UP929312/pbp",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Flake8",
+    ],
+    entry_points={
+        'flake8.extension': [
+            'PBP1 = src.flake8_pbp:ProduceBetterPythonPlugin',
+        ],
+    },
+)
