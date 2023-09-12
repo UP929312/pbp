64c72e568c10f41b3966063693057e1b12a23685
UP929312 <36114463+UP929312@users.noreply.github.com>
2023-09-12 14:29:37 +0100

    Improved test autogeneration

--- generate_test.py
+++ generate_test.py
@@ -7,18 +7,16 @@ final_string = "\n".join(
         "condition = True",
         "my_list = [1, 2, 3]",
         "my_map = {1: 'a', 2: 'b'}",
+        "",
+        "",
     ]
 )
 
 for check in all_checks:
     docs: str = check.__doc__.replace("\n    ", "\n")
-    start, end = docs.split("# Good")
-    good = end.strip("").strip("\n")
-    bad = start.strip("").strip("\n").removeprefix("# Bad\n")
-
-    final_string += f"# {check.__name__}\n\n{bad}\n\n{good}\n\n"
+    final_string += f"#========== {check.__name__}\n{docs}\n"
 
 with open("src/tests/main_test.py", "w") as file:
-    file.write("import requests\n"+final_string)
+    file.write(final_string)
 
 print(r"flake8 --ignore=R,E,W,F .\src\tests\main_test.py")
--- src/tests/main_test.py
+++ src/tests/main_test.py
@@ -1,134 +1,167 @@
 import requests
-import requests
 
 condition = True
 my_list = [1, 2, 3]
-my_map = {1: 'a', 2: 'b'}# RangeLenNotAllowed
+my_map = {1: 'a', 2: 'b'}
 
+#========== RangeLenNotAllowed
+
+# Bad
 for i in range(len(my_list)):
     print(my_list[i])
 
+# Good
 for x in list:
     print(x)
 
-# JsonLoadsNotAllowed
+#========== JsonLoadsNotAllowed
 
+# Bad
 import json
 response = requests.get("https://example.com")
 data = json.loads(response.text)
 
+# Good
 response = requests.get("https://example.com").json()
 
-# OpenNoWithNotAllowed
+#========== OpenNoWithNotAllowed
 
+# Bad
 file = open("file.txt")
 file.read()
 file.close()
 
+# Good
 with open("file.txt") as file:
     data = file.read()
 
-# RequestsJsonDumpsNotAllowed
+#========== RequestsJsonDumpsNotAllowed
 
+# Bad
 import json
 requests.post(data=json.dumps(data))
 
+# Good
 requests.post(json=data)
 
-# AssignToListNotAllowed
+#========== AssignToListNotAllowed
 
+# Bad
 x = list()
 
+# Good
 x = []
 
-# CamelCaseFuncNotAllowed
+#========== CamelCaseFuncNotAllowed
 
+# Bad
 def CamelCaseFunc():
     pass
 
+# Good
 def snake_case_func():
     pass
 
-# DefaultMutableArgsNotAllowed
+#========== DefaultMutableArgsNotAllowed
 
+# Bad
 def foo(bar=[]):
     pass
 
+# Good
 def foo(bar=None):
     if bar is None:
         bar = []
     pass
 
-# CompareTypesNotAllowed
+#========== CompareTypesNotAllowed
 
+# Bad
 if type(x) == int:
     pass
 if type(x) is int:
     pass
 
+# Good
 if isinstance(x, int):
     pass
 
-# ComparedToTrueNotAllowed
+#========== ComparedToTrueNotAllowed
 
+# Bad
 if x is True:
     pass
 
+# Good
 if x:
     pass
 
-# InheritsFromObjectNotAllowed
+#========== InheritsFromObjectNotAllowed
 
+# Bad
 class Foo(object):
     pass
 
+# Good
 class Foo:
     pass
 
-# NotUsingTernaryNotAllowed
+#========== NotUsingTernaryNotAllowed
 
+# Bad
 if x == 1:
     a = "Value1"
 else:
     a = "Value2"
 
+# Good
 a = "Value1" if x == 1 else "Value2"
 
-# UsingFilterNotAllowed
+#========== UsingFilterNotAllowed
 
+# Bad
 list(filter(lambda x: x > 5, range(10)))
 list(map(lambda x: x**2, range(10)))
 
+# Good
 [x for x in range(10) if x > 5]
 [x**2 for x in range(10)]
 
-# ShadowBuiltinsNotAllowed
+#========== ShadowBuiltinsNotAllowed
 
+# Bad
 map = my_map
 list = my_list
 
+# Good
 my_map = my_map
 my_list = my_list
 
-# NotPointlessTernaryNotAllowed
+#========== NotPointlessTernaryNotAllowed
 
+# Bad
 x = True if condition else False
 
+# Good
 x = bool(condition)
 
-# AnyWithCompNotAllowed
+#========== AnyWithCompNotAllowed
 
+# Bad
 any([x.id for x in my_list])
 all([x.id for x in my_list])
 
+# Good
 all(x.id for x in my_list)
 any(x.id for x in my_list)
 
-# NonPascalCaseClassNotAllowed
+#========== NonPascalCaseClassNotAllowed
 
+# Bad
 class my_class:
     pass
 
+# Good
 class MyClass:
     pass
 
